7905a8f20eaa311aca7b8c73f2d8f665d0b68ee6
Rina Antropova <rin.antropova@gmail.com>
2024-07-30 09:54:00 +0300

    Seminar 2, Files extensions, my solution

--- sem_2/task6.java
+++ sem_2/task6.java
@@ -7,5 +7,39 @@
 
 package sem_2;
 
+import java.nio.file.Path;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+
 public class task6 {
+    public static void printFileExtensions () {
+        try {
+            Path currentDir = Paths.get(".");
+            File[] files = currentDir.toFile().listFiles();
+
+            if (files != null) {
+                int count = 1;
+                for (File file : files) {
+                    if (file.isFile()) {
+                        String name = file.getName();
+                        int lastIndexOf = name.lastIndexOf(".");
+                        String extension = lastIndexOf > 0 ? name.substring(lastIndexOf + 1) : "";
+
+                        System.out.println(count + "File extension " + extension);
+                        count++;
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        printFileExtensions();
+    }
 }
